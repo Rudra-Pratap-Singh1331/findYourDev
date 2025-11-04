7168a7a87d470a48094d7db080637f75f463cae8
Rudra Pratap Singh <rudrapsingh121@gmail.com>
2025-11-04 16:08:44 +0530

    udpated

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -10,9 +10,7 @@ const authRouter = express.Router();
 authRouter.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ email }).select(
-      "fullName techStack designation profileUpdateStatus photoUrl mobileNumber age email"
-    );
+    const user = await User.findOne({ email });
     if (!user)
       return res
         .status(400)
